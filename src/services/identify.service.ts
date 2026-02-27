6fa49d06730a40dd362885cf345d8125ae9a0509
reetambg <r33t4m@gmail.com>
2026-02-27 11:50:06 +0530

    added merge logic for multiple primary contacts

--- src/services/identify.service.ts
+++ src/services/identify.service.ts
@@ -1,5 +1,5 @@
 import { prisma } from "../lib/prisma.js";
-import { LinkPrecedence } from "../generated/prisma/client.js";
+import { LinkPrecedence, type Contact } from "../generated/prisma/client.js";
 
 export const createNewContact = async (
   email: string,
@@ -60,24 +60,29 @@ export const findContacts = async (email: string, phoneNumber: string) => {
     }
   }
 
+  let canonicalPrimaryId: number;
+
   if (primaryIds.size > 1) {
-    // TODO: merge needed
-    console.log("Merge required for contacts: ", contacts);
-    throw new Error("Multiple primary contacts found, merge required. This is not implemented yet.");
+    // merge primaries if multiple primary contacts found
+    const oldestPrimaryId = await mergePrimaries(Array.from(primaryIds));
+    canonicalPrimaryId = oldestPrimaryId;
+  } else {
+    if (primaryIds.size === 0) {
+      throw new Error("Data inconsistency: no primary ID found");
+    }
+    canonicalPrimaryId = Array.from(primaryIds)[0]!;
   }
 
   const primaryContact = await prisma.contact.findFirst({
     where: {
       deletedAt: null,
-      id: { in: Array.from(primaryIds) },
-    },
-    orderBy: {
-      createdAt: "asc",
+      id: canonicalPrimaryId,
     },
   });
 
   if (!primaryContact) {
-    // TODO: create a new contact if no primary contact found, but for now just throw an error
+    // This should never happen, data inconsistency if it does
+    // as we have already verified existence of primary contacts above
     throw new Error("No primary contact found");
   }
 
@@ -88,8 +93,85 @@ export const findContacts = async (email: string, phoneNumber: string) => {
     },
   });
 
-  // check if a contact with same email or phone number exists in the fullGroup
-  // if yes then do nothing else create a new secondary contact linked to the primary contact
+  await createSecondaryIfNecessary(
+    email,
+    phoneNumber,
+    primaryContact,
+    fullGroup,
+  );
+
+  // construct response
+  const res = buildResponse(primaryContact, fullGroup);
+
+  console.log("Matched contact:", contacts);
+  console.log("Primary contact IDs:", primaryIds);
+  console.log("Chosen primary: ", primaryContact);
+  console.log("Full group: ", fullGroup);
+
+  return res;
+};
+
+// merge primaries
+// if multiple primaries found, link them to the oldest primary and convert them to secondaries
+const mergePrimaries = async (primaryIds: number[]) => {
+  const primaryContacts = await prisma.contact.findMany({
+    where: {
+      deletedAt: null,
+      id: { in: primaryIds },
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+  });
+
+  if (primaryContacts.length === 0) {
+    throw new Error(
+      "Data inconsistency: primary contacts not found for all primary IDs",
+    );
+  }
+
+  const oldestPrimary = primaryContacts[0];
+  if (!oldestPrimary) {
+    throw new Error("Oldest primary contact not found");
+  }
+
+  const contactsToUpdate = primaryContacts.slice(1); // all except the oldest primary
+
+  // TODO: start DB transaction
+  // Transaction needed to ensure that all updates happen atomically,
+  // preventing data inconsistency as we are updating multiple records together
+
+  const result = await prisma.$transaction(async (tx) => {
+    for (let contact of contactsToUpdate) {
+      // convert duplicate primaries to secondaries
+      // link them to the oldest primary
+      await tx.contact.update({
+        where: { id: contact.id },
+        data: {
+          linkedId: oldestPrimary.id,
+          linkPrecedence: LinkPrecedence.secondary,
+        },
+      });
+
+      // relink any secondaries linked to the duplicate primary to the oldest primary
+      await tx.contact.updateMany({
+        where: { linkedId: contact.id },
+        data: { linkedId: oldestPrimary.id },
+      });
+    }
+  });
+
+  return oldestPrimary.id;
+};
+
+// check if a contact with same email or phone number exists in the fullGroup
+// if yes then do nothing else create a new secondary contact linked to the primary contact
+const createSecondaryIfNecessary = async (
+  email: string,
+  phoneNumber: string,
+  primaryContact: Contact,
+  fullGroup: Contact[],
+) => {
   const alreadyExists = fullGroup.some(
     (c) =>
       (email ? c.email === email : true) && // true if email is not provided
@@ -106,8 +188,10 @@ export const findContacts = async (email: string, phoneNumber: string) => {
 
     fullGroup.push({ ...newSecondary, linkedId: primaryContact.id }); // add the new secondary contact to the full group for response construction
   }
+};
 
-  // construct response
+// Build response object
+const buildResponse = (primaryContact: Contact, fullGroup: Contact[]) => {
   const primaryContactId = primaryContact.id;
   const emails: string[] = []; // first element being primary contact's email
   const phoneNumbers: string[] = []; // first element being primary contact's phone number
@@ -134,7 +218,7 @@ export const findContacts = async (email: string, phoneNumber: string) => {
   phoneNumbers.push(...secondaryPhoneNumbers);
   const secondaryContactIdsArray = Array.from(secondaryContactIds);
 
-  const res = {
+  return {
     contact: {
       primaryContactId,
       emails,
@@ -142,11 +226,4 @@ export const findContacts = async (email: string, phoneNumber: string) => {
       secondaryContactIds: secondaryContactIdsArray,
     },
   };
-
-  console.log("Matched contact:", contacts);
-  console.log("Primary contact IDs:", primaryIds);
-  console.log("Chosen primary: ", primaryContact);
-  console.log("Full group: ", fullGroup);
-
-  return res;
 };
